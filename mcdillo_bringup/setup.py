3cc22bc8e4bc094727fd91e396392b22db790988
Rodrigo Lopes Catto <rodrigo.catto7@gmail.com>
2023-12-14 19:48:16 -0300

    Add Ydlidar package

--- mcdillo_bringup/setup.py
+++ mcdillo_bringup/setup.py
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['launch/ydlidar_launch.py']),
+        ('share/' + package_name, ['params/X4.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
